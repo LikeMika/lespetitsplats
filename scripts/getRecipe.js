3053cdbc80adc0abcbf5b330ab1adc57ab5e9b8d
Michael Bricout <bricout.michael@gmail.com>
2025-02-18 18:21:54 -0500

    Recipe card design completed + display of all recipe

---
+++ scripts/getRecipe.js
@@ -0,0 +1,84 @@
+console.log(recipes);
+const recipeResultContainer = document.getElementById('recipe-result');
+
+function loadRecipe() {
+    recipes.forEach(element => {
+        const recipeContainer = document.createElement('div');
+        recipeContainer.classList.add('col-md-4');
+        recipeContainer.classList.add('p3');
+        recipeContainer.classList.add('recipe');
+
+
+        //image
+        const recipeImage = document.createElement('img')
+        recipeImage.src = `assets/recipes/${element.image}`;
+        recipeImage.classList.add('image-recipe');
+
+        const contentContainer = document.createElement('div');
+        contentContainer.classList.add('recipe-content');
+
+        //title
+        const recipeTitle = document.createElement('h3');
+        recipeTitle.textContent = element.name;
+
+        // séparateur RECETTE
+        const recipeSeparator = document.createElement('span');
+        recipeSeparator.classList.add('separator');
+        recipeSeparator.textContent = 'RECETTE';
+        
+        // description recette
+        const recipeDescription = document.createElement('p');
+        recipeDescription.classList.add('recipe-description');
+        recipeDescription.textContent = element.description;
+
+        // séparateur INGREDIENTS
+        const ingredientSeparator = document.createElement('span');
+        ingredientSeparator.classList.add('separator');
+        ingredientSeparator.textContent = 'INGRÉDIENTS';
+
+        //container des ingrédients
+        const ingredientContainer = document.createElement('div');
+        ingredientContainer.classList.add('row');
+        ingredientContainer.classList.add('ingredient-container');
+
+        element.ingredients.forEach(item => {
+            const ingredientColumn = document.createElement('div');
+            ingredientColumn.classList.add('col-md-6');
+            ingredientColumn.classList.add('single-ingredient');
+
+            const ingredientName = document.createElement('span');
+            ingredientName.classList.add('nom-ingredient');
+            ingredientName.textContent = item.ingredient;
+
+            const ingredientDose = document.createElement('span');
+            ingredientDose.classList.add('dose-ingredient');
+            ingredientDose.textContent = item.quantity;
+            if(item.unit)
+            {
+                ingredientDose.textContent = item.quantity+item.unit;
+            }
+            else if(!item.quantity)
+            {
+                ingredientDose.textContent = "-";
+            }
+
+            ingredientColumn.appendChild(ingredientName);
+            ingredientColumn.appendChild(ingredientDose);
+
+            ingredientContainer.appendChild(ingredientColumn);
+        })
+
+        recipeContainer.appendChild(recipeImage);
+        contentContainer.appendChild(recipeTitle);
+        contentContainer.appendChild(recipeSeparator);
+        contentContainer.appendChild(recipeDescription);
+        contentContainer.appendChild(ingredientSeparator);
+        contentContainer.appendChild(ingredientContainer);
+
+        recipeContainer.appendChild(contentContainer);
+        recipeResultContainer.appendChild(recipeContainer);
+        
+    });
+}
+
+loadRecipe();
